4221c98c108de80c6522267096b16ddf23b8c5f5
Max G J Panas <m@maxpanas.com>
2015-05-25 01:10:15 +0300

    untested debouncer rewrite

--- assets/js/_debouncer.js
+++ assets/js/_debouncer.js
@@ -25,32 +25,34 @@ class Debouncer {
 
     // register listener
     if (window.addEventListener) {
-      target.addEventListener(events, bouncer, false);
+      target.addEventListener(events, self.bouncer.bind(self), false);
     } else if (window.attachEvent) {
-      target.attachEvent(`on${events}`, bouncer);
+      target.attachEvent(`on${events}`, self.bouncer.bind(self));
     } else {
-      target[`on${events}`] = bouncer;
-    }
-
-    // /////////////////////////////
-
-    /**
-     * Run all attached callbacks
-     * if there aren't any already
-     * running
-     *
-     *
-     */
-    function bouncer(event) {
-      if (0 < self.callbacks.length && !self.running) {
-        self.running = true;
-        window.requestAnimationFrame(() => {
-          self.callbacks.forEach(cb => cb(event));
-          self.running = false;
-        });
-      }
+      target[`on${events}`] = self.bouncer.bind(self);
     }
   }
+
+
+  /**
+   * Run all attached callbacks
+   * if there aren't any already
+   * running
+   *
+   *
+   */
+  bouncer(event) {
+    let self = this;
+
+    if (0 < self.callbacks.length && !self.running) {
+      self.running = true;
+      window.requestAnimationFrame(() => {
+        self.callbacks.forEach(cb => cb(event));
+        self.running = false;
+      });
+    }
+  }
+
 
   /**
    * Attach a new callback
