6bd478e43727195d98030c7923bfe389a8d6ef8e
Max GJ Panas <m@maxpanas.com>
2015-05-25 17:49:20 +0300

    refactor debouncer to add more useful methods and performance optimizations

--- assets/js/_debouncer.js
+++ assets/js/_debouncer.js
@@ -3,92 +3,169 @@
  *
  */
 class Debouncer {
-  /**
-   * Register an event
-   * Debounce handler on
-   * a specific target
-   *
-   * @param events string
-   * @param maybeTarget
-   */
-  constructor(events, maybeTarget) {
-    let self = this;
-    let target = maybeTarget || window;
+	/**
+	 * Register an events
+	 * Debounce handler on
+	 * a specific target
+	 *
+	 * @param events string
+	 * @param target
+	 */
+	constructor(events, target) {
+		let self = this;
 
-    if ('string' !== typeof events) {
-      throw new Error('Debouncer: Debouncer can only debounce events of type string');
-    }
+		// properties
+		self.target = target || window;
+		self.callbacks = [];
+		self.running = false;
 
-    // properties
-    self.callbacks = [];
-    self.running = false;
-
-    // register listener
-    if (window.addEventListener) {
-      target.addEventListener(events, self.bouncer.bind(self), false);
-    } else if (window.attachEvent) {
-      target.attachEvent(`on${events}`, self.bouncer.bind(self));
-    } else {
-      target[`on${events}`] = self.bouncer.bind(self);
-    }
-  }
+		self.addListeners(events);
+	}
 
 
-  /**
-   * Run all attached callbacks
-   * if there aren't any already
-   * running
-   *
-   *
-   */
-  bouncer(event) {
-    let self = this;
+	/**
+	 * Register the Debouncer to
+	 * one or more events
+	 *
+	 * @param events string
+	 */
+	addListeners(events) {
+		let self = this;
 
-    if (0 < self.callbacks.length && !self.running) {
-      self.running = true;
-      window.requestAnimationFrame(() => {
-        self.callbacks.forEach(cb => cb(event));
-        self.running = false;
-      });
-    }
-  }
+		if ('string' !== typeof events) {
+			throw new TypeError('Debouncer: Debouncer can only debounce events of type string');
+		}
 
+		self.events = self.events ? self.events.split(' ').concat(events.split(' ')).join(' ') : events;
 
-  /**
-   * Attach a new callback
-   * to the Debouncer
-   *
-   * @param cb
-   * @returns {Number}
-   */
-  register(cb) {
-    if ('function' !== typeof cb) {
-      throw new Error('Debouncer: registered callback must be callable');
-    }
-    return this.callbacks.push(cb);
-  }
+		if (window.addEventListener) {
+			self.target.addEventListener(events, self._bouncer.bind(self), false);
+		} else {
+			let eventsList = events.split(' ').map(event => `on${event}`);
+
+			if (window.attachEvent) {
+				self.target.attachEvent(eventsList.join(' '), self._bouncer.bind(self));
+			} else {
+				eventsList.forEach(event => self.target[event] = self._bouncer.bind(self));
+			}
+		}
+	}
 
 
-  /**
-   * Remove a callback from
-   * the array of callbacks
-   * attached to the
-   * Debouncer
-   *
-   * @param cb
-   * @returns {Number}
-   */
-  deregister(cb) {
-    if ('function' !== typeof cb) {
-      throw new Error('Debouncer: registered callback must be callable');
-    }
+	/**
+	 * Deregister the Debouncer
+	 * from one or more events
+	 *
+	 * @param events string
+	 * @param hard bool (default:false) totally deregister events from the Debouncer
+	 */
+	removeListeners(events, hard) {
+		let self = this;
 
-    let indexOfCb = this.callbacks.indexOf(cb);
-    if (-1 < indexOfCb) {
-      this.callbacks.splice(indexOfCb, 1);
-    }
-    return indexOfCb;
-  }
+		if ('string' !== typeof events) {
+			throw new TypeError('Debouncer: Debouncer can only debounce events of type string');
+		}
+
+		if (hard) {
+			// if hard option enabled then remove
+			// the events from the "events" property
+			// of the Debouncer
+			let registered = self.events.split(' ');
+
+			events.split(' ').forEach(event => {
+				let indexOfEvent = registered.indexOf(event);
+				if (-1 < indexOfEvent) {
+					registered.splice(indexOfEvent, 1);
+				}
+			});
+
+			self.events = registered.join(' ');
+		}
+
+		if (window.removeEventListener) {
+			self.target.removeEventListener(events, self._bouncer.bind(self), false);
+		} else {
+			let eventsList = events.split(' ').map(event => `on${event}`);
+
+			if (window.detachEvent) {
+				self.target.detachEvent(eventsList.join(' '), self._bouncer.bind(self));
+			} else {
+				eventsList.forEach(event => self.target[event] = null);
+			}
+		}
+	}
+
+
+	/**
+	 * Run all attached callbacks
+	 * if there aren't any already
+	 * running
+	 *
+	 * @private
+	 * @param event
+	 */
+	_bouncer(event) {
+		let self = this;
+
+		if (self.callbacks.length && !self.running) {
+			self.running = true;
+			window.requestAnimationFrame(() => {
+				self.callbacks.forEach(cb => cb(event));
+				self.running = false;
+			});
+		}
+	}
+
+
+	/**
+	 * Attach a new callback
+	 * to the Debouncer
+	 *
+	 * @param callback
+	 * @returns {Number}
+	 */
+	register(callback) {
+		let self = this;
+
+		if ('function' !== typeof callback) {
+			throw new TypeError('Debouncer: registered callback must be callable');
+		}
+
+		if (!self.callbacks.length) {
+			self.addListeners(self.events);
+		}
+
+		return self.callbacks.push(callback);
+	}
+
+
+	/**
+	 * Remove a callback from
+	 * the array of callbacks
+	 * attached to the
+	 * Debouncer
+	 *
+	 * @param callback
+	 * @returns {Number}
+	 */
+	deregister(callback) {
+		let self = this;
+
+		if ('function' !== typeof callback) {
+			throw new TypeError('Debouncer: registered callback must be callable');
+		}
+
+		let indexOfCb = self.callbacks.indexOf(callback);
+		if (-1 < indexOfCb) {
+			self.callbacks.splice(indexOfCb, 1);
+		}
+
+		if (!self.callbacks.length) {
+			self.removeListeners(self.events);
+		}
+
+		return indexOfCb;
+	}
 }
 
 export default Debouncer;
